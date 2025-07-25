35eb069f741fa05ae33ef1b9081af65c3ab1eb47
Matthew Evans <mevans+git@wtfsystems.net>
2025-07-25 11:03:33 -0400

    sync

--- CREDITS.md
+++ CREDITS.md
@@ -1,1 +1,3 @@
-Tileset: <https://m039.itch.io/blue-space-cave-tileset>
+<https://fontenddev.com/fonts/poco/>
+<https://m039.itch.io/blue-space-cave-tileset>
+<https://flatus.itch.io/waste-no-space-gui>
---
+++ theme/gen-theme.gd
@@ -0,0 +1,19 @@
+@tool
+extends ProgrammaticTheme
+
+var default_font = "res://theme/font/Poco.ttf"
+var default_font_size = 16
+
+var text_font_color = Color.WHITE
+
+func setup():
+	set_save_path("res://theme//theme.tres")
+
+func define_theme():
+	define_default_font(ResourceLoader.load(default_font))
+	define_default_font_size(default_font_size)
+	
+	define_style("Label", {
+		font_color = text_font_color,
+		line_spacing = default_font_size
+	})
