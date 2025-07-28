3845955bffa552c9677e3b3e805a7417a809b647
Matthew Evans <mevans+git@wtfsystems.net>
2025-07-28 14:01:22 -0400

    sync

--- scripts/autostart/globals.gd
+++ scripts/autostart/globals.gd
@@ -1,13 +1,5 @@
 extends Node
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func alert(text: String) -> void:
 	var dialog = AcceptDialog.new()
 	dialog.dialog_text = text
