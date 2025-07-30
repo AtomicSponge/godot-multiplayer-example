7e13602ed038d94d0aa43f5e7b6194f1d2e7d060
Matthew Evans <mevans+git@wtfsystems.net>
2025-07-30 16:08:11 -0400

    sync

---
+++ scripts/autostart/scene_manager.gd
@@ -0,0 +1,30 @@
+extends Node
+
+var Scenes: Dictionary = {}
+
+@onready var Fader = $Fader/AnimationPlayer
+
+# Switch to the requested scene based on its alias
+func SwitchScene(sceneAlias: String) -> void:
+	get_tree().paused = true
+	Fader.play("Fade")
+	await Fader.animation_finished
+	get_tree().change_scene_to_file(Scenes[sceneAlias])
+	Fader.play_backwards("Fade")
+	await Fader.animation_finished
+	get_tree().paused = false
+
+# Restart the current scene
+func RestartScene() -> void:
+	get_tree().paused = true
+	Fader.play("Fade")
+	await Fader.animation_finished
+	get_tree().reload_current_scene()
+	Fader.play_backwards("Fade")
+	await Fader.animation_finished
+	get_tree().paused = false
+
+# Quit the game
+func QuitGame() -> void:
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
