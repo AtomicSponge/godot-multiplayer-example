4fe56330293aa065fe55c307754740fef1cbb96c
Matthew Evans <mevans+git@wtfsystems.net>
2025-07-31 12:49:38 -0400

    sync

--- scripts/main_ui.gd
+++ scripts/main_ui.gd
@@ -1,10 +1,14 @@
 extends Control
 
 func _on_host_game_btn_pressed() -> void:
-	SceneManager.SwitchScene("lobby_scene")
+	#SceneManager.SwitchScene("lobby_scene")
+	NetworkHandler.start_server()
+	SceneManager.SwitchScene("world_scene")
 
 func _on_join_game_btn_pressed() -> void:
-	SceneManager.SwitchScene("search_scene")
+	#SceneManager.SwitchScene("search_scene")
+	NetworkHandler.start_client()
+	SceneManager.SwitchScene("world_scene")
 
 func _on_settings_btn_pressed() -> void:
 	SceneManager.SwitchScene("settings_scene")
