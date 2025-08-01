ba6eeb2963e62422891e3e5a3a4ffcb1e67b9e25
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-01 11:53:21 -0400

    sync

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,20 +3,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-var owner_id: int
-var is_authority: bool:
-	get: return !NetworkHandler.is_server && owner_id == ClientNetworkGlobals.id
-
 func _enter_tree() -> void:
-	ServerNetworkGlobals.handle_player_position.connect(server_handler_player_position)
-	ClientNetworkGlobals.handle_player_position.connect(client_handle_player_position)
-
-func _exit_tree() -> void:
-	ServerNetworkGlobals.handle_player_position.disconnect(server_handler_player_position)
-	ClientNetworkGlobals.handle_player_position.disconnect(client_handle_player_position)
+	set_multiplayer_authority(name.to_int())
 
 func _physics_process(delta: float) -> void:
-	if !is_authority: return
+	if !is_multiplayer_authority(): return
 
 	# Add the gravity.
 	if not is_on_floor():
@@ -35,17 +26,3 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
-	PlayerPosition.create(owner_id, global_position).send(NetworkHandler.server_peer)
-
-func server_handler_player_position(peer_id: int, player_position: PlayerPosition) -> void:
-	if owner_id != peer_id: return
-
-	global_position = player_position.position
-
-	PlayerPosition.create(owner_id, global_position).broadcast(NetworkHandler.connection)
-
-func client_handle_player_position(player_position: PlayerPosition) -> void:
-	if is_authority || owner_id != player_position.id: return
-
-	global_position = player_position.position
