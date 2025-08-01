fd4412f95ed39d41558a2dbb825024714190bcfd
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-01 15:27:11 -0400

    sync

--- scripts/autostart/network_handler.gd
+++ scripts/autostart/network_handler.gd
@@ -2,15 +2,23 @@ extends Node
 
 const IP_ADDRESS: String = "localhost"
 const PORT: int = 42069
+const MAX_PLAYERS = 8
 
 var peer: ENetMultiplayerPeer
 
-func start_server() -> void:
+func start_server() -> Error:
 	peer = ENetMultiplayerPeer.new()
-	peer.create_server(PORT)
+	var error: Error = peer.create_server(PORT, 8)
+	if error: return error
 	multiplayer.multiplayer_peer = peer
+	return OK
 
-func start_client() -> void:
+func start_client() -> Error:
 	peer = ENetMultiplayerPeer.new()
-	peer.create_client(IP_ADDRESS, PORT)
+	var error: Error = peer.create_client(IP_ADDRESS, PORT)
+	if error: return error
 	multiplayer.multiplayer_peer = peer
+	return OK
+
+func close_connection() -> void:
+	multiplayer.multiplayer_peer = null
--- scripts/player.gd
+++ scripts/player.gd
@@ -7,7 +7,7 @@ func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
 
 func _physics_process(delta: float) -> void:
-	if !is_multiplayer_authority(): return
+	if not is_multiplayer_authority(): return
 
 	# Add the gravity.
 	if not is_on_floor():
