0b127e2e1dc9fb352cb994d5b30c2a7cd36ac973
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-05 14:40:01 -0400

    sync

---
+++ scripts/main_ui.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_host_game_btn_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_join_game_btn_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_settings_btn_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_btn_pressed() -> void:
+	Globals.quit_game()
