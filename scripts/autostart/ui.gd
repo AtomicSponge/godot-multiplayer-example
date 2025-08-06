20890bc4623601141a29396756dea3739ec4a934
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-06 08:25:59 -0400

    sync

--- scripts/autostart/ui.gd
+++ scripts/autostart/ui.gd
@@ -1,5 +1,12 @@
 extends Control
 
-signal menu_opened()
-signal menu_closed()
-signal quit_to_menu()
+const _interfaces: Dictionary[String, PackedScene] = {
+	"HostUI": preload("res://uis/host_ui.tscn"),
+	"LobbyUI": preload("res://uis/lobby_ui.tscn"),
+	"MainUI": preload("res://uis/main_ui.tscn"),
+	"SearchUI": preload("res://uis/search_ui.tscn"),
+	"SettingsUI": preload("res://uis/settings_ui.tscn")
+}
+
+func open_menu() -> void:
+	pass
