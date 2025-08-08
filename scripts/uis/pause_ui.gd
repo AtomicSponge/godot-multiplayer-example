433212868318b2755e10f66fc63229bd368b92f7
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-08 08:46:50 -0400

    sync

--- scripts/uis/pause_ui.gd
+++ scripts/uis/pause_ui.gd
@@ -1,1 +1,10 @@
 extends Control
+
+func _on_resume_game_btn_pressed() -> void:
+	UiController.close_menu()
+
+func _on_settings_btn_pressed() -> void:
+	UiController.open_menu("SettingsUI")
+
+func _on_quit_btn_pressed() -> void:
+	Globals.quit_game()
