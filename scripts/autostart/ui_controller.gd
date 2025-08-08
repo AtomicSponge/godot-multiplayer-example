29045db27638ea107ff96e4b2986c6ad640dccce
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-08 09:49:37 -0400

    sync

--- scripts/autostart/ui_controller.gd
+++ scripts/autostart/ui_controller.gd
@@ -3,11 +3,11 @@ extends Node
 var _ui_node: Node
 var _ui_ref: Array[String]
 
-const _menus: Dictionary[String, PackedScene] = {
+const menus: Dictionary[String, PackedScene] = {
+	"GameUI": preload("res://uis/game_ui.tscn"),
 	"HostUI": preload("res://uis/host_ui.tscn"),
 	"LobbyUI": preload("res://uis/lobby_ui.tscn"),
 	"MainUI": preload("res://uis/main_ui.tscn"),
-	"PauseUI": preload("res://uis/pause_ui.tscn"),
 	"SearchUI": preload("res://uis/search_ui.tscn"),
 	"SettingsUI": preload("res://uis/settings_ui.tscn")
 }
@@ -15,7 +15,7 @@ const _menus: Dictionary[String, PackedScene] = {
 func open_menu(menu_name: String) -> void:
 	_clear_menu_mem()
 	_ui_ref.push_back(menu_name)
-	_ui_node.add_child(_menus[menu_name].instantiate())
+	_ui_node.add_child(menus[menu_name].instantiate())
 
 func close_menu() -> void:
 	_clear_menu_mem()
