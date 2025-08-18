485eb63b04d4fcb6d40dbf4d06a08806ac7af360
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-18 12:27:11 -0400

    sync

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,20 +1,29 @@
 extends Node2D
 
 @onready var PlayerList: Node = $PlayerList
+@onready var PlayerSpawner: MultiplayerSpawner = $PlayerSpawner
 
 var PLAYER: PackedScene = preload("res://scenes/player.tscn")
 
-func _spawn_player(id: int) -> void:
+func _set_player_position(player: Node) -> void:
+	if player.is_inside_tree():
+		player.global_position = Vector2(randi() % 401 + 400, 350)
+
+func _make_player(data:Dictionary) -> Node:
 	var player: Node = PLAYER.instantiate()
-	player.name = str(id)
-	player.global_position = Vector2(randi() % 401 + 400, 350)
-	PlayerList.call_deferred("add_child", player)
+	player.name = str(data.id)
+	call_deferred("_set_player_position", player)
+	return player
+
+func _spawn_player(id: int) -> void:
+	PlayerSpawner.spawn({ "id": id })
 
 func _remove_player(id: int) -> void:
 	if not PlayerList.has_node(str(id)): return
 	PlayerList.get_node(str(id)).queue_free()
 
 func _ready() -> void:
+	PlayerSpawner.set_spawn_function(_make_player)
 	if not multiplayer.is_server(): return
 	multiplayer.peer_connected.connect(_spawn_player)
 	multiplayer.peer_disconnected.connect(_remove_player)
