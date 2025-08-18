cd9f30e3179aa727aa046a6d7bd13f01018535dc
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-18 14:44:20 -0400

    sync

---
+++ scripts/console.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+@onready var ConsoleWindow: RichTextLabel = $ConsoleWindow
+@onready var ConsoleInput: LineEdit = $ConsoleInput
+
+func _ready() -> void:
+	pass # Replace with function body.
