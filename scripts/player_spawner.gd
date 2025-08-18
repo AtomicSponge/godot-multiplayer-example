193f302d5a2121eb7e856d4aac3e783e2ce2a8dd
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-18 12:42:12 -0400

    sync

--- scripts/player_spawner.gd
+++ scripts/player_spawner.gd
@@ -5,7 +5,7 @@ var PLAYER: PackedScene = preload("res://scenes/player.tscn")
 func _make_player(data:Dictionary) -> Node:
 	var player: Node = PLAYER.instantiate()
 	player.name = str(data.id)
-	player.position = Vector2(randi() % 201 + 700, 350)
+	player.position = Vector2(randi() % 701 + 200, 350)
 	return player
 
 func _ready() -> void:
