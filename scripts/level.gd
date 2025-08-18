e935b407c434f3f8d0940e7b4a6f3a323c2694a9
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-18 11:06:35 -0400

    sync

---
+++ scripts/level.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var PlayerList: Node = $PlayerList
+
+var PLAYER: PackedScene = preload("res://scenes/player.tscn")
+
+func _spawn_player(id: int) -> void:
+	var player: Node = PLAYER.instantiate()
+	player.name = str(id)
+	player.position = Vector2(randi() % 401 + 400, 350)
+	PlayerList.call_deferred("add_child", player)
+
+func _remove_player(id: int) -> void:
+	if not PlayerList.has_node(str(id)): return
+	PlayerList.get_node(str(id)).queue_free()
+
+func _ready() -> void:
+	if not multiplayer.is_server(): return
+	multiplayer.peer_connected.connect(_spawn_player)
+	multiplayer.peer_disconnected.connect(_remove_player)
+
+	for id in multiplayer.get_peers():
+		_spawn_player(id)
+
+	if not OS.has_feature("dedicated_server"):
+		_spawn_player(1)
