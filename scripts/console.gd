b549773fe79ccd6938b6c9d488c5ed8b52facd2e
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-19 08:40:51 -0400

    sync

--- scripts/console.gd
+++ scripts/console.gd
@@ -1,14 +1,28 @@
 extends CanvasLayer
 
-@onready var ConsoleWindow: RichTextLabel = $VBoxContainer/ConsoleWindow
-@onready var ConsoleInput: LineEdit = $VBoxContainer/ConsoleInput
+@onready var ConsoleWindow: RichTextLabel = $ConsoleContainer/ConsoleWindow
+@onready var ConsoleInput: LineEdit = $ConsoleContainer/ConsoleInput
 
 func _ready() -> void:
 	pass
+
+func _on_console_input_text_submitted(new_text: String) -> void:
+	if new_text == "": return
+	ConsoleWindow.add_text(new_text + "\n")
+	ConsoleInput.clear()
+	ConsoleInput.has_focus()
+	ConsoleInput.call_deferred("edit")
+
+func _on_console_input_text_changed(new_text: String) -> void:
+	if visible and Input.is_action_just_pressed("console"):
+		hide()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("console") and Input.is_action_just_pressed("console"):
 		if not visible:
 			show()
+			ConsoleInput.clear()
+			ConsoleInput.has_focus()
+			ConsoleInput.call_deferred("edit")
 		else:
 			hide()
