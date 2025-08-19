52793de13e746e9205017b696e7338476508868e
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-19 10:04:58 -0400

    sync

--- addons/console/console.gd
+++ addons/console/console.gd
@@ -4,6 +4,16 @@ extends CanvasLayer
 @onready var ConsoleWindow: RichTextLabel = $ConsoleContainer/ConsoleWindow
 @onready var ConsoleInput: LineEdit = $ConsoleContainer/ConsoleInput
 
+func add_text(new_text: String) -> void:
+	ConsoleWindow.add_text(new_text)
+
+func set_window_size(new_size: Vector2) -> void:
+	ConsoleContainer.size.x = new_size.x
+	ConsoleWindow.custom_minimum_size.y = new_size.y
+
+func set_position(new_position: Vector2) -> void:
+	ConsoleContainer.position = new_position
+
 func _on_console_input_text_submitted(new_text: String) -> void:
 	ConsoleInput.clear()
 	ConsoleInput.has_focus()
--- addons/console/plugin.gd
+++ addons/console/plugin.gd
@@ -7,8 +7,6 @@ const PATH = "res://addons/console/"
 
 func _enter_tree() -> void:
 	add_autoload_singleton(AUTOLOAD_NAME, str(PATH + "console.tscn"))
-	#add_custom_type(NODE_NAME, "Node", preload(PATH + "ui_anchor.gd"), preload(PATH + "icon.svg"))
 
 func _exit_tree() -> void:
 	remove_autoload_singleton(AUTOLOAD_NAME)
-	#remove_custom_type(NODE_NAME)
