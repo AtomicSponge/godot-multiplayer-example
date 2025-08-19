c3191e9abab047de005ae33b9353614dde88666f
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-19 13:28:50 -0400

    sync

--- addons/console/console.gd
+++ addons/console/console.gd
@@ -4,31 +4,35 @@ extends CanvasLayer
 @onready var ConsoleWindow: RichTextLabel = $ConsoleContainer/ConsoleWindow
 @onready var ConsoleInput: LineEdit = $ConsoleContainer/ConsoleInput
 
-var command_table: Dictionary[String, Callable] = {}
+var _command_table: Dictionary[String, Callable] = {}
 
+## Add text to the console window.
 func add_text(new_text: String) -> void:
 	if ConsoleWindow == null: return
 	ConsoleWindow.add_text(new_text + "\n")
 
+## Set the console window size.
 func set_window_size(new_size: Vector2) -> void:
 	ConsoleContainer.size.x = new_size.x
 	ConsoleWindow.custom_minimum_size.y = new_size.y
 
+## Set the position of the console.
 func set_position(new_position: Vector2) -> void:
 	ConsoleContainer.position = new_position
 
+## Add a new command to the console
 func add_command(command: String, callback: Callable) -> void:
-	command_table[command] = callback
+	_command_table[command] = callback
 
-func process_command(command: String) -> void:
+func _process_command(command: String) -> void:
 	if not command.begins_with("/"): return
 	var cmd_split: Array = command.split(" ", false, 1)
 	var cmd = cmd_split[0].lstrip("/")
 	var arg = ""
 	if cmd_split.size() >= 2:
 		arg = cmd_split[1]
-	if command_table.has(cmd):
-		command_table[cmd].call(arg)
+	if _command_table.has(cmd):
+		_command_table[cmd].call(arg)
 	else:
 		add_text("Command not found.")
 
@@ -37,7 +41,7 @@ func _on_console_input_text_submitted(new_text: String) -> void:
 	ConsoleInput.has_focus()
 	ConsoleInput.call_deferred("edit")
 	if new_text == "": return
-	process_command(new_text)
+	_process_command(new_text)
 
 func _on_console_input_text_changed(_new_text: String) -> void:
 	if visible and Input.is_action_just_pressed("console"):
