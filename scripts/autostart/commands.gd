9c669ea72e280b6b3167d458993cc2a8de81c153
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-19 12:47:30 -0400

    sync

--- scripts/autostart/commands.gd
+++ scripts/autostart/commands.gd
@@ -3,5 +3,9 @@ extends Node
 func say_command(text: String) -> void:
 	Console.add_text(text)
 
+func quit_command(_arg: String) -> void:
+	Globals.quit_game()
+
 func _enter_tree() -> void:
+	Console.add_command("quit", quit_command)
 	Console.add_command("say", say_command)
