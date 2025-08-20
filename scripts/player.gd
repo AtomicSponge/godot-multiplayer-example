789a913b7112ac1cd3ec0bad119ae1ddc1569112
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-20 11:56:12 -0400

    sync

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var PlayerCamera: Camera2D = $PlayerCamera
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -800.0
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
