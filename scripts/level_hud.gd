cecc70ea33e53c9d39cee2c4e53ffa7c3c52345a
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-21 15:35:58 -0400

    sync

--- scripts/level_hud.gd
+++ scripts/level_hud.gd
@@ -3,6 +3,25 @@ extends CanvasLayer
 @onready var BottomLabel: Label = $BottomLabel
 @onready var ScorePanel: Panel = $ScorePanel
 
+@onready var ScorePanelPlayerRef: Array[Label] = [
+	$ScorePanel/Player1,
+	$ScorePanel/Player2,
+	$ScorePanel/Player3,
+	$ScorePanel/Player4,
+	$ScorePanel/Player5,
+	$ScorePanel/Player6,
+	$ScorePanel/Player7,
+	$ScorePanel/Player8,
+	$ScorePanel/Player9,
+	$ScorePanel/Player10,
+	$ScorePanel/Player11,
+	$ScorePanel/Player12,
+	$ScorePanel/Player13,
+	$ScorePanel/Player14,
+	$ScorePanel/Player15,
+	$ScorePanel/Player16,
+]
+
 func _update_bottom_label(new_text: String) -> void:
 	BottomLabel.show()
 	BottomLabel.set_text(new_text)
@@ -11,6 +30,13 @@ func _update_bottom_label(new_text: String) -> void:
 
 func _ready() -> void:
 	EventBus.ShowKill.connect(_update_bottom_label)
+
+	for element in ScorePanelPlayerRef:
+		element.text = ""
+
+func _process(_delta: float) -> void:
+	for counter in Globals.LOBBY_MEMBERS.size():
+		ScorePanelPlayerRef[counter].text = Globals.LOBBY_MEMBERS[counter].steam_name
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("score_panel") and Input.is_action_just_pressed("score_panel"):
