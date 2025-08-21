0ae7b6accf2967df1318878e4a55a01ef0fa7532
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-21 08:52:35 -0400

    sync

--- scripts/player.gd
+++ scripts/player.gd
@@ -50,7 +50,6 @@ func _unhandled_input(event: InputEvent) -> void:
 		Globals.GAME_MENU_OPENED = false
 		UiController.close_all_menus()
 		return
-
 	if event.is_action_pressed("game_menu") and Input.is_action_just_pressed("game_menu") and not Globals.GAME_MENU_OPENED:
 		Globals.GAME_MENU_OPENED = true
 		UiController.open_menu("GameUI")
