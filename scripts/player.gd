aa31db044856e9cd999647772f94735488aa90ca
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-22 08:52:53 -0400

    sync

--- scripts/player.gd
+++ scripts/player.gd
@@ -39,7 +39,7 @@ func _input(_event: InputEvent) -> void:
 
 func _physics_process(delta: float) -> void:
 	if not NetworkHandler.is_network_connected(): return
-	if not is_multiplayer_authority(): return
+	if not multiplayer.is_server(): return
 
 	# Add the gravity.
 	if not is_on_floor():
