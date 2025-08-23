8a728170afe902ab54be2ded9694f344fbe74173
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-22 22:06:30 -0400

    sync

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,8 @@
 extends Node
 
 @onready var Level: Node = $Level
+@onready var PlayerList: Node = $PlayerList
+@onready var PlayerSpawner: MultiplayerSpawner = $PlayerSpawner
 
 ##  Start a game and if server replicate the level
 func start_game():
@@ -8,9 +10,16 @@ func start_game():
 	if multiplayer.is_server():
 		load_level.call_deferred(load("res://scenes/level.tscn"))
 
-@rpc("authority", "call_remote", "reliable")
-func disconnect_all_players() -> void:
-	EventBus.EndGame.emit("Server disconnected from the game!")
+	if not multiplayer.is_server(): return
+	multiplayer.peer_connected.connect(spawn_player)
+	multiplayer.peer_disconnected.connect(remove_player)
+	multiplayer.server_disconnected.connect(remove_player)
+
+	for id in multiplayer.get_peers():
+		spawn_player(id)
+
+	if not OS.has_feature("dedicated_server"):
+		spawn_player(1)
 
 ##  End the game and close the network connection
 func end_game(why: String = ""):
@@ -31,6 +40,19 @@ func load_level(scene: PackedScene) -> void:
 		Level.remove_child(node)
 		node.queue_free()
 	Level.add_child(scene.instantiate())
+
+##  Spawn a player
+func spawn_player(id: int) -> void:
+	PlayerSpawner.spawn({ "id": id, "position": Vector2(randi() % 701 + 200, 350) })
+
+##  Remove a player
+func remove_player(id: int) -> void:
+	if not PlayerList.has_node(str(id)): return
+	PlayerList.get_node(str(id)).queue_free()
+
+@rpc("authority", "call_remote", "reliable")
+func disconnect_all_players() -> void:
+	EventBus.EndGame.emit("Server disconnected from the game!")
 
 func _ready() -> void:
 	EventBus.StartGame.connect(start_game)
