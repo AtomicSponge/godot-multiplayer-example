cf006bce5f324cb2ac86328aabeff485e9b34d1c
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-25 08:52:34 -0400

    sync

--- addons/fps_label/fps_label.gd
+++ addons/fps_label/fps_label.gd
@@ -1,20 +1,40 @@
 extends Label
 ## A simple node for displaying the FPS.
+## Also includes a powermode that shows process time
+
+var power_mode: bool = false
 
 ## Show the FPS Label.
 func show_fps() -> void:
+	power_mode = false
+	show()
+
+## Show the FPS Label in powermode.
+func show_powermode() -> void:
+	power_mode = true
 	show()
 
 ## Hide the FPS Label.
 func hide_fps() -> void:
 	hide()
 
+func _ready() -> void:
+	hide()
+
 func _process(delta: float) -> void:
-	set_text("FPS:  %d" % Engine.get_frames_per_second())
+	if power_mode:
+		set_text("FPS:  %d" % Engine.get_frames_per_second() +
+			"\nCPU:  %f" % delta)
+	else:
+		set_text("FPS:  %d" % Engine.get_frames_per_second())
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("fps") and Input.is_action_just_pressed("fps"):
 		if not visible:
 			show_fps()
 		else:
-			hide_fps()
+			if not power_mode:
+				power_mode = true
+			else:
+				power_mode = false
+				hide_fps()
