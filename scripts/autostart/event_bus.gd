a40ef3b60bcce0d43663091ae99980887b4eff05
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-26 10:37:11 -0400

    sync

--- scripts/autostart/event_bus.gd
+++ scripts/autostart/event_bus.gd
@@ -1,8 +1,8 @@
 extends Node
 
-signal StartGame  # Start the game
-signal EndGame(why: String)    # End the game
+signal StartGame  ## Start the game
+signal EndGame(why: String)    ## End the game
 
-signal UpdatePlayerName  # Called when your Steam name is updated
+signal UpdatePlayerName  ## Called when your Steam name is updated
 
-signal ShowKill(text: String)  # Show kill message
+signal ShowKill(text: String)  ## Show kill message
