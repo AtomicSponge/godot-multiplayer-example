bc7946a76b2076b5c5966295febef48fb97350e2
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-29 10:44:58 -0400

    sync

---
+++ scripts/weapons/gun.gd
@@ -0,0 +1,7 @@
+extends Weapon
+
+func _ready() -> void:
+	damage = 100
+
+func _process(_delta: float) -> void:
+	pass
---
+++ scripts/weapons/weapon.gd
@@ -0,0 +1,11 @@
+extends Node2D
+class_name Weapon
+
+var damage: int = 0		## Amount of damage the weapon does
+var ammo: int = 0		## How much ammo the weapon has (-1 it does not use ammo)
+
+func _ready() -> void:
+	pass
+
+func _process(_delta: float) -> void:
+	pass
