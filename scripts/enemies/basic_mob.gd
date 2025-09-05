a719a1d0d507c41342b53149a5a31ac1959381f0
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-05 09:18:20 -0400

    sync

--- scripts/enemies/basic_mob.gd
+++ scripts/enemies/basic_mob.gd
@@ -1,5 +1,12 @@
 class_name BasicMob extends RigidBody2D
 
+const SPEED: float = 350.0
+const CHASE_SPEED: float = 500.0
+
+enum States { WALKING, CHASING, ATTACKING }
+
+var state: States = States.WALKING
+
 func _ready() -> void:
 	pass
 
