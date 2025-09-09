90f762765483577d03008651203f119f4dd2a2ff
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-09 12:41:10 -0400

    sync

--- scripts/enemies/basic_mob.gd
+++ scripts/enemies/basic_mob.gd
@@ -3,9 +3,9 @@ class_name BasicMob extends CharacterBody2D
 const WALK_SPEED: float = 350.0
 const CHASE_SPEED: float = 500.0
 
-enum States { WALKING, CHASING, ATTACKING }
+enum MovementStates { WALKING, CHASING, ATTACKING }
 
-var _state: States = States.WALKING
+var moveState: MovementStates = MovementStates.WALKING
 
 func _ready() -> void:
 	pass
@@ -17,13 +17,13 @@ func _physics_process(delta: float) -> void:
 	var direction_x: float = -1
 	var direction_y: float = -1
 
-	if _state == States.WALKING:
+	if moveState == MovementStates.WALKING:
 		velocity.x = direction_x * WALK_SPEED
 		velocity.y = direction_y * WALK_SPEED
-	elif _state == States.CHASING:
+	elif moveState == MovementStates.CHASING:
 		velocity.x = direction_x * CHASE_SPEED
 		velocity.y = direction_y * CHASE_SPEED
-	elif _state == States.ATTACKING:
+	elif moveState == MovementStates.ATTACKING:
 		velocity.x = move_toward(velocity.x, 0, WALK_SPEED)
 		velocity.y = move_toward(velocity.y, 0, WALK_SPEED)
 
