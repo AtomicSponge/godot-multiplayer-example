143d0c86aacccf9f3b277914c95cf026e1230d6b
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-09 11:00:56 -0400

    sync

--- scripts/players/player.gd
+++ scripts/players/player.gd
@@ -5,6 +5,13 @@ class_name Player extends CharacterBody2D
 
 const SPEED: float = 500.0
 
+enum MovementStates { IDLE, WALKING }
+
+var moveState: MovementStates = MovementStates.IDLE
+
+var direction_x: float = 0.0
+var direction_y: float = 0.0
+
 ##  Update the player display name
 func update_player_name() -> void:
 	NameLabel.set_text(Globals.NAME)
@@ -23,23 +30,23 @@ func _input(_event: InputEvent) -> void:
 
 	# Stop the player if the menu or console is opened
 	if GameState.GAME_MENU_OPENED or Console.is_opened():
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		moveState = MovementStates.IDLE
 		return
 
 	# Get the input direction and handle the movement/deceleration
-	var direction_x: float = Input.get_axis("move_left", "move_right")
-	if direction_x:
-		velocity.x = direction_x * SPEED
+	direction_x = Input.get_axis("move_left", "move_right")
+	direction_y = Input.get_axis("move_up", "move_down")
+	if direction_x != 0.0 or direction_y != 0.0:
+		moveState = MovementStates.WALKING
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	var direction_y: float = Input.get_axis("move_up", "move_down")
-	if direction_y:
-		velocity.y = direction_y * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		moveState = MovementStates.IDLE
 
 func _physics_process(_delta: float) -> void:
 	if not is_multiplayer_authority(): return
+	if moveState == MovementStates.IDLE:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	elif moveState == MovementStates.WALKING:
+		velocity.x = direction_x * SPEED
+		velocity.y = direction_y * SPEED
 	move_and_slide()
