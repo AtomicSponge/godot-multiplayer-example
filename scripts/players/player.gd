81d3130c936fdb811bda0e157c18eda1b23291fe
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-16 17:34:32 -0400

    sync

--- scripts/players/player.gd
+++ scripts/players/player.gd
@@ -24,33 +24,6 @@ func _ready() -> void:
 	EventBus.UpdatePlayerName.connect(update_player_name)
 	NameLabel.set_text(Globals.NAME)
 
-func _input(_event: InputEvent) -> void:
-	return
-	if not is_multiplayer_authority(): return
-
-	# Stop input handling if the menu or console is opened
-	if GameState.GAME_MENU_OPENED or Console.is_opened():
-		velocity.x = move_toward(velocity.x, 0, current_speed)
-		velocity.y = move_toward(velocity.y, 0, current_speed)
-		return
-
-	if Input.is_action_pressed("run"):
-		current_speed = RUN_SPEED
-	else:
-		current_speed = WALK_SPEED
-
-	# Get the input direction and handle the movement/deceleration
-	var direction_x: float = Input.get_axis("move_left", "move_right")
-	if direction_x:
-		velocity.x = direction_x * current_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, current_speed)
-	var direction_y: float = Input.get_axis("move_up", "move_down")
-	if direction_y:
-		velocity.y = direction_y * current_speed
-	else:
-		velocity.y = move_toward(velocity.y, 0, current_speed)
-
 func _physics_process(_delta: float) -> void:
 	# Stop input handling if the menu or console is opened
 	if GameState.GAME_MENU_OPENED or Console.is_opened():
