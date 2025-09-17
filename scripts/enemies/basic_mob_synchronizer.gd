2f9b0a8cf264772b0504381253957b2917198ce1
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-17 14:03:05 -0400

    sync

--- scripts/enemies/basic_mob_synchronizer.gd
+++ scripts/enemies/basic_mob_synchronizer.gd
@@ -1,6 +1,6 @@
 extends MultiplayerSynchronizer
 
-@onready var basicMob = $".."
+@onready var basicMob = get_parent()
 
 func _on_chase_area_body_entered(body: Node2D) -> void:
 	if not is_inside_tree() or not multiplayer.has_multiplayer_peer() or not is_multiplayer_authority():
