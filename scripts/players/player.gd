a1b63f6bb31d1a67e44415909cf8e7e9cc935469
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-17 08:12:34 -0400

    sync

--- scripts/players/player.gd
+++ scripts/players/player.gd
@@ -3,11 +3,11 @@ class_name Player extends CharacterBody2D
 @onready var PlayerCamera: Camera2D = $PlayerCamera
 @onready var NameLabel: Label = $NameLabel
 
-@onready var input = $InputSynchronizer
-
 const WALK_SPEED: float = 450.0
 const RUN_SPEED: float = 650.0
+var direction: Vector2 = Vector2()
 var current_speed: float = WALK_SPEED
+var running: bool = false
 
 ##  Update the player display name
 func update_player_name() -> void:
@@ -18,11 +18,13 @@ func _enter_tree() -> void:
 
 func _ready() -> void:
 	PlayerCamera.enabled = is_multiplayer_authority()
-	input.set_multiplayer_authority(name.to_int())
-	set_physics_process(multiplayer.is_server())
 	if not is_multiplayer_authority(): return
 	EventBus.UpdatePlayerName.connect(update_player_name)
 	NameLabel.set_text(Globals.NAME)
+
+func _process(delta: float) -> void:
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	running = Input.is_action_pressed("run")
 
 func _physics_process(_delta: float) -> void:
 	# Stop input handling if the menu or console is opened
@@ -31,11 +33,10 @@ func _physics_process(_delta: float) -> void:
 		velocity.y = move_toward(velocity.y, 0, current_speed)
 		return
 
-	if input.running:
+	if running:
 		current_speed = RUN_SPEED
 	else:
 		current_speed = WALK_SPEED
-	var direction = input.direction
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.y = direction.y * current_speed
