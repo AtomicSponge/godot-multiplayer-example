b092714b9b77c985cc6b35c7c524517f5e9b40da
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-21 09:52:24 -0400

    sync

--- scripts/autostart/event_bus.gd
+++ scripts/autostart/event_bus.gd
@@ -4,5 +4,3 @@ signal StartGame  ## Start the game
 signal EndGame(why: String)    ## End the game
 
 signal UpdatePlayerName  ## Called when your Steam name is updated
-
-signal ShowKill(text: String)  ## Show kill message
