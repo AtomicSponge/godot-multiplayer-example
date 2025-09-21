efe744ddf182e5e8bd7a35f2fbe21c77ff6634ee
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-21 10:41:48 -0400

    sync

--- scripts/enemies/basic_mob_synchronizer.gd
+++ scripts/enemies/basic_mob_synchronizer.gd
@@ -11,5 +11,5 @@ func _on_chase_area_body_entered(body: Node2D) -> void:
 func _on_loss_area_body_exited(body: Node2D) -> void:
 	if not is_inside_tree() or not multiplayer.has_multiplayer_peer() or not is_multiplayer_authority():
 		return
-	if body is Player:
+	if body == basicMob.targetPlayer:
 		basicMob.set_target_player(null)
