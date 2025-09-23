2fc41f189b162dd57f8122e8907b0d788ffb3881
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-23 07:13:19 -0400

    sync

--- scripts/players/player.gd
+++ scripts/players/player.gd
@@ -41,6 +41,8 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if not is_multiplayer_authority(): return
+	# Stop input if the menu or console is opened
+	if GameState.GAME_MENU_OPENED or Console.is_opened(): return
 
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction:
