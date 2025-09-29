3fb99389a76808d3348169dd9b821de11eb90db7
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-29 10:40:10 -0400

    sync

--- scripts/players/bullet.gd
+++ scripts/players/bullet.gd
@@ -5,9 +5,11 @@ class_name Bullet extends Area2D
 var speed = 750
 
 func _ready() -> void:
+	NetworkTime.on_tick.connect(_on_tick)
 	LifeTimer.start()
 
-func _process(delta: float) -> void:
+#func _process(delta: float) -> void:
+func _on_tick(delta: float, _tick: float) -> void:
 	if LifeTimer.is_stopped():
 		queue_free()
 	position += transform.x * speed * delta
