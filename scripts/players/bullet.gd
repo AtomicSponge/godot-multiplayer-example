83ce5415f23a1b09a0f748be3a8ed00ef103b232
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-29 11:01:33 -0400

    sync

--- scripts/players/bullet.gd
+++ scripts/players/bullet.gd
@@ -5,10 +5,9 @@ class_name Bullet extends Area2D
 var speed = 750
 
 func _ready() -> void:
-	NetworkTime.on_tick.connect(_on_tick)
 	LifeTimer.start()
 
-func _on_tick(delta: float, _tick: float) -> void:
+func _process(delta: float) -> void:
 	if LifeTimer.is_stopped():
 		queue_free()
 	position += transform.x * speed * delta
--- scripts/players/bullet_synchronizer.gd
+++
@@ -1,8 +0,0 @@
-extends MultiplayerSynchronizer
-
-func _on_bullet_body_entered(body: Node2D) -> void:
-	if body is Player:
-		return
-	if body is Enemy:
-		body.queue_free()
-	queue_free()
