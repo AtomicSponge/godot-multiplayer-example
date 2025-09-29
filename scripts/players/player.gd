18117cbca477503cde0b3ef0d5147bbb5e720aed
Matthew Evans <mevans+git@wtfsystems.net>
2025-09-29 10:01:57 -0400

    sync

--- scripts/players/player.gd
+++ scripts/players/player.gd
@@ -48,7 +48,6 @@ func fire_weapon() -> void:
 	var b: Bullet = bullet.instantiate()
 	add_child(b)
 	b.global_position = FireLocation.global_position
-	#b.rotation = WeaponSprite.rotation
 	b.look_at(input.mousePosition)
 
 ##  Run animations.
