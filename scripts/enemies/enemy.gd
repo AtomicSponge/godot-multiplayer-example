187ba52b6a843daae9af8925692f5e5366d527d6
Matthew Evans <mevans+git@wtfsystems.net>
2025-10-01 09:14:47 -0400

    sync

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -19,5 +19,5 @@ func change_state(old_state: MovementStates, new_state: MovementStates) -> Movem
 	moveState = new_state
 	return old_state
 
-func _enter_tree() -> void:
-	set_multiplayer_authority(1, true)
+#func _enter_tree() -> void:
+	#set_multiplayer_authority(1, true)
