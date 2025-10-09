bc02b7378001657f112c249351ac123bf5a69c19
Matthew Evans <mevans+git@wtfsystems.net>
2025-10-09 08:20:26 -0400

    sync

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -5,6 +5,7 @@ extends MultiplayerSpawner
 func _make_enemy(data: Dictionary) -> Node:
 	var enemy: Node = load(ENEMIES[data.type]).instantiate()
 	enemy.position = data.position
+	#enemy.set_multiplayer_authority(1)
 	return enemy
 
 func _enter_tree() -> void:
