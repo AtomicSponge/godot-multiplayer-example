b48021a7381bcf8392c5e8e99647afd04579fa00
Matthew Evans <mevans+git@wtfsystems.net>
2025-10-10 06:49:54 -0400

    sync

--- scripts/players/bullet.gd
+++ scripts/players/bullet.gd
@@ -1,17 +1,21 @@
 class_name Bullet extends Area2D
 
-@onready var LifeTimer = $LifeTimer
+var speed: float = 400.0
+var distance: float = 20000.0
 
-var speed = 750
+var distance_left: float = 0.0
 
 func _ready() -> void:
-	LifeTimer.start()
+	NetworkTime.on_tick.connect(_tick)
+	distance_left = distance
 
 #func _process(delta: float) -> void:
-func _rollback_tick(delta: float, _tick: float, _is_fresh: bool) -> void:
-	if LifeTimer.is_stopped():
-		queue_free()
+func _tick(delta: float, _t: float) -> void:
 	position += transform.x * speed * delta
+	distance_left -= speed
+
+	if distance_left < 0:
+		queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
 	if not is_inside_tree() or not multiplayer.has_multiplayer_peer() or not is_multiplayer_authority():
