bce2b91215e914d7e3900e98e503c5c301642934
Matthew Evans <mevans+git@wtfsystems.net>
2026-01-06 13:18:59 -0500

    sync

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -2,9 +2,6 @@ extends CanvasLayer
 
 @onready var Console: CanvasLayer = $Console
 
-################################################################################
-#   Console commands														   #
-################################################################################
 ##  Broadcast a message to chat
 func say_command(text: String) -> void:
 	var sent: bool = Steam.sendLobbyChatMsg(Globals.LOBBY_ID, text)
@@ -16,16 +13,9 @@ func say_command(text: String) -> void:
 ##  Quit the game from command
 func quit_command(_arg: String) -> void:
 	Globals.quit_game()
-################################################################################
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Console.set_console_size(Vector2(550, 200))
 	Console.set_position(Vector2(2, 482))
 	Console.add_command("say", say_command)
 	Console.add_command("quit", quit_command)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
